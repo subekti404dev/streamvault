6551f84e1b328b32d88551828276e7db6d34af4d
Urip Subekti <urip.subekti13@gmail.com>
2026-06-21 00:28:21 +0700

    chore: add JetBrains Mono and Inter font dependencies

--- dashboard/src/main.ts
+++ dashboard/src/main.ts
@@ -1,3 +1,9 @@
+import '@fontsource/jetbrains-mono/700.css';
+import '@fontsource/jetbrains-mono/600.css';
+import '@fontsource/jetbrains-mono/500.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
 import { mount } from 'svelte';
 import App from './App.svelte';
 import './app.css';
